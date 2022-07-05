8bb9cf01129006d44baf9c1f679d0062607a4311
MykolaSirkoWork <mykola.sirko.work@gmail.com>
2022-07-05 10:41:25 +0300

    stripe

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path')
 const bodyParser = require("body-parser")
 const mongoose = require("./database");
 const session = require("express-session");
+const stripe = require("stripe")('sk_test_51LHjpdEZZiK54waal5CeD2qHjc9P5LV7sUqFgUsJ8Vi8EwSkNzGD1XQBEVPCxcKcgabBa8WxdUmWryAs6evDl0Ra00vjb96Cqe');
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
 const io = require("socket.io")(server, {pingTimeout: 60000});
@@ -32,7 +33,7 @@ const uploadRoute = require('./routes/uploadRoutes');
 const searchRoute = require('./routes/searchRoutes');
 const messagesRoute = require('./routes/messagesRoutes');
 const notificationsRoute = require('./routes/notificationRoutes');
-
+const Stripe = require('./routes/api/stripe');
 // Api routes
 const postsApiRoute = require('./routes/api/posts');
 const usersApiRoute = require('./routes/api/users');
@@ -69,38 +70,36 @@ app.use("/api/chats", middleware.requireLogin, chatsApiRoute);
 app.use("/api/messages", middleware.requireLogin, messagesApiRoute);
 app.use("/api/notifications", middleware.requireLogin, notificationsApiRoute);
 app.use("/api/comments", middleware.requireLogin, commentsApiRoute);
+app.use('/create-payment-intent', middleware.requireLogin, Stripe);
 
 
 
 
 
-const stripe = require("stripe")('sk_test_51LHjpdEZZiK54waal5CeD2qHjc9P5LV7sUqFgUsJ8Vi8EwSkNzGD1XQBEVPCxcKcgabBa8WxdUmWryAs6evDl0Ra00vjb96Cqe');
+// app.use(express.static("public"));
+// app.use(express.json());
 
-app.use(express.static("public"));
-app.use(express.json());
+// const calculateOrderAmount = (items) => {
+//   return 1400;
+// };
 
-const calculateOrderAmount = (items) => {
-  return 1400;
-};
+// app.post("/create-payment-intent", async (req, res) => {
+//   const { items } = req.body;
 
-app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
+//   // Create a PaymentIntent with the order amount and currency
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: calculateOrderAmount(items),
+//     currency: "eur",
+//     automatic_payment_methods: {
+//       enabled: true,
+//     },
+//   });
 
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "eur",
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
+//   res.send({
+//     clientSecret: paymentIntent.client_secret,
+//   });
+// });
 
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
-
-app.listen(4242, () => console.log("Node server listening on port 4242!"));
 
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
