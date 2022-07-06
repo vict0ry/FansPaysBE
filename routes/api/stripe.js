7f84fa01e285517416f256037ec23bce984e7c26
MykolaSirkoWork <mykola.sirko.work@gmail.com>
2022-07-06 10:57:19 +0300

    trying stripe

--- routes/api/stripe.js
+++ routes/api/stripe.js
@@ -11,16 +11,36 @@ const calculateOrderAmount = (items) => {
 };
 
 router.post("/create", async (req, res) => {
-  const { items } = req.body;
-
+  // const { items } = req.body;
+  const customer = await stripe.customers.create();
+  await stripe.customers
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "usd",
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
+  customer: customer.id,
+  setup_future_usage: 'off_session',
+  amount: 1099,
+  currency: 'eur',
+  automatic_payment_methods: {
+    enabled: true,
+  },
+});
+
+// try {
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: 1099,
+//     currency: 'usd',
+//     customer: '{{CUSTOMER_ID}}',
+//     payment_method: '{{PAYMENT_METHOD_ID}}',
+//     off_session: true,
+//     confirm: true,
+//   });
+// } catch (err) {
+//   // Error code will be authentication_required if authentication is needed
+//   console.log('Error code is: ', err.code);
+//   const paymentIntentRetrieved = await stripe.paymentIntents.retrieve(err.raw.payment_intent.id);
+//   console.log('PI retrieved: ', paymentIntentRetrieved.id);
+// }
+console.log(paymentIntent, 'cskdmaksdkasmkd');
 
   res.send({
     clientSecret: paymentIntent.client_secret,
